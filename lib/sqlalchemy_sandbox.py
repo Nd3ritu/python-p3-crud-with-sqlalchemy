edb0f7aa35e7c58ad9845aa6d4d44c37a93c060d
Matthew <+>
2023-12-13 10:24:50 +0300

    finished crud with sqlachemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,10 +12,65 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name= 'id_pk'),
+        UniqueConstraint(
+            'email',
+            name = 'unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name ='grade_between_1_and_12')
+    )
+
+    Index('index_name', 'name')
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default = datetime.now())
+
+    def __repr__(self):
+        return  f"Student(id={self.id}, name={self.name}, email={self.email}, grade={self.grade})"
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind = engine)
+    session = Session()
+
+    albert_einstein = Student(
+        name = "Albert Einstein",
+        email = "albert.einstein@zurich.edu",
+        grade = 6,
+        birthday = datetime(
+            year=1879,
+            month=3,
+            day=14
+        ),
+    )
+
+    alan_turing = Student(
+        name= "Alan Turing",
+        email= "alan.truing@sherborne.edu",
+        grade = 11,
+        birthday = datetime(
+            year=1912,
+            month=6,
+            day=23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    students = session.query(Student)
+    print([student for student in students])
+
+    # print(f"New student ID is {albert_einstein.id}.")
+    # print(f"New student ID is {alan_turing.id}.")
+
